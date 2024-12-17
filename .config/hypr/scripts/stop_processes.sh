20d1c0430d13dc7af3614ecd312d26c6a95ebc91
Viktor Holta <viktor.holta@outlook.com>
2024-12-17 07:53:32 +0100

    Split hyprland window/workspace settings by device

--- .config/hypr/scripts/stop_processes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROCESS_ADDRESSES=$(hyprctl clients -j | jq '.[] | .address?' -r)
-for ADDRESS in $PROCESS_ADDRESSES
-do
-	#hyprctl dispatch closewindow address:$ADDRESS
-	echo $ADDRESS
-done
